4136e6dbe85699f383f13d2dd0c4010cdbfce2a1
xianyu <2538420009@qq.com>
2023-06-25 15:27:26 +0800

    修改Swagger配置

--- power-common/common-core/src/main/java/com/xianyu/config/SwaggerAutConfiguration.java
+++ power-common/common-core/src/main/java/com/xianyu/config/SwaggerAutConfiguration.java
@@ -1,5 +1,6 @@
 package com.xianyu.config;
 
+import com.xianyu.constant.AuthConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -7,12 +8,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+
+import static com.xianyu.constant.AuthConstant.AUTHORIZATION;
+import static com.xianyu.constant.AuthConstant.LOGIN_TYPE;
 
 /**
  * @BelongsProject: powershop
@@ -47,7 +53,9 @@ public class SwaggerAutConfiguration {
                 .enable(flag)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
-                .build();
+                .build()
+                .securityContexts(securityContexts())
+                .securitySchemes(securitySchemes());
     }
 
     private ApiInfo apiInfo(){
@@ -68,4 +76,35 @@ public class SwaggerAutConfiguration {
         );
     }
 
+    private List<SecurityScheme> securitySchemes(){
+        List<SecurityScheme> schemes = new ArrayList<>();
+        schemes.add(new ApiKey(AUTHORIZATION,AUTHORIZATION,"header"));
+        schemes.add(new ApiKey(LOGIN_TYPE,LOGIN_TYPE,"header"));
+        return schemes;
+    }
+
+    private List<SecurityContext> securityContexts(){
+        List<SecurityContext> contexts = new ArrayList<>();
+        contexts.add(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .operationSelector(o->o.requestMappingPattern().matches("/.*"))
+                        .build()
+        );
+        return contexts;
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope =new AuthorizationScope("global","accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> securityReferences =new ArrayList<>();
+        securityReferences.add(new SecurityReference(AUTHORIZATION,authorizationScopes));
+
+        AuthorizationScope globalAuthorizationScope = new AuthorizationScope("global","accessEverything");
+        AuthorizationScope[] globalAuthorizationScopes =new AuthorizationScope[1];
+        globalAuthorizationScopes[0] =globalAuthorizationScope;
+        securityReferences.add(new SecurityReference(LOGIN_TYPE,globalAuthorizationScopes));
+        return securityReferences;
+    }
 }
